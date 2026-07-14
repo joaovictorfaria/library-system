dc7ef2a1c126d32b9c1c7c7d82849953182d4981
joaovictorfaria <joao.fsantana@outlook.com>
2026-07-14 17:43:51 -0300

    feat: validate Book creation

--- backend/Library.Domain/Entities/Book.cs
+++ backend/Library.Domain/Entities/Book.cs
@@ -1,21 +1,24 @@
-﻿using System;
+﻿using Library.Domain.Exceptions;
+using System;
 
 namespace Library.Domain.Entities;
 
-    public class Book
-    {
-        public Guid Id { get; private set; }
-        public string Title { get; private set; }
-        public string AuthorName { get; private set; }
-        public string Isbn { get; private set; }
-        public int PublicationYear { get; private set; }
+public class Book
+{
+    public Guid Id { get; private set; }
+    public string Title { get; private set; }
+    public string AuthorName { get; private set; }
+    public string Isbn { get; private set; }
+    public int PublicationYear { get; private set; }
 
     public Book(
-        string title,
-        string authorName,
-        string isbn,
-        int publicationYear)
+      string title,
+      string authorName,
+      string isbn,
+      int publicationYear)
     {
+        Validate(title, authorName, isbn, publicationYear);
+
         Id = Guid.NewGuid();
 
         Title = title;
@@ -24,5 +27,34 @@ namespace Library.Domain.Entities;
         PublicationYear = publicationYear;
     }
 
+    private void Validate(
+        string title,
+        string authorName,
+        string isbn,
+        int publicationYear)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+        {
+            throw new DomainException("Title is required.");
+        }
+
+        if (string.IsNullOrWhiteSpace(authorName))
+        {
+            throw new DomainException("Author name is required.");
+        }
+
+        if (string.IsNullOrWhiteSpace(isbn))
+        {
+            throw new DomainException("ISBN is required.");
+        }
+
+        if (publicationYear <= 0 || publicationYear > DateTime.Now.Year)
+        {
+            throw new DomainException("Publication year is invalid.");
+        }
     }
+
+
+
+}
 
---
+++ backend/Library.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Library.Domain.Exceptions;
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base(message) 
+        {
+            
+        }
+
+    }
+
