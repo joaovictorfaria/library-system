bd991c0e18e3d86264bf7b21ca2c130dce661fa3
joaovictorfaria <joao.fsantana@outlook.com>
2026-07-14 16:13:16 -0300

    feat: add initial Book entity

---
+++ backend/Library.Domain/Entities/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Library.Domain.Entities;
+
+    public class Book
+    {
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string AuthorName { get; private set; }
+        public string Isbn { get; private set; }
+        public int PublicationYear { get; private set; }
+
+    public Book(
+        string title,
+        string authorName,
+        string isbn,
+        int publicationYear)
+    {
+        Id = Guid.NewGuid();
+
+        Title = title;
+        AuthorName = authorName;
+        Isbn = isbn;
+        PublicationYear = publicationYear;
+    }
+
+    }
+
