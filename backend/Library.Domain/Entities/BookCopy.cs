287fd82136f237608795210dbb99ecc00d15717e
joaovictorfaria <joao.fsantana@outlook.com>
2026-07-15 18:58:11 -0300

    feat: add BookCopy entity

---
+++ backend/Library.Domain/Entities/BookCopy.cs
@@ -0,0 +1,32 @@
+﻿using Library.Domain.Enums;
+using Library.Domain.Exceptions;
+using System;
+
+
+namespace Library.Domain.Entities;
+
+public class BookCopy
+{
+    public Guid Id { get; private set; }
+    public Guid BookId { get; private set; }
+    public BookCopyStatus Status { get; private set; }
+
+    public BookCopy(Guid bookId)
+    {
+        Validate(bookId);
+
+        Id = Guid.NewGuid();
+        BookId = bookId;
+        Status = BookCopyStatus.Available;
+    }
+
+    private static void Validate(Guid bookId)
+    {
+        if (bookId == Guid.Empty)
+        {
+            throw new DomainException("BookId is required.");
+        }
+    }
+
+}
+
---
+++ backend/Library.Domain/Enums/BookCopyStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Library.Domain.Enums;
+
+    public enum BookCopyStatus
+    {
+        Available,
+        Borrowed,
+        Reserved,
+        Maintenance
+    }
+
